061d5c1d692d6281e35726866f1c7d157e041f39
Frederick Johnson <fritzj96@gmail.com>
2020-01-28 08:36:02 -0800

    added: error handling middleware

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,2 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 import { HttpException } from '../exceptions/http.exception';
+
+export const errorMiddleware = (
+  error: HttpException,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const status = error.status || 500;
+  const message =
+    error.message || 'Something Has Gone Wrong. Contact the Admin.';
+  response.status(status).json({
+    message,
+    status
+  });
+};
