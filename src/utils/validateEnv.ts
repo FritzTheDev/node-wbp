2fd2fecdd35a8af6effdb2cf7984fc6bdf7f0f34
Frederick Johnson <fritzj96@gmail.com>
2020-01-29 15:19:59 -0800

    updated: validateEnv to use postgres vars

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,11 +1,13 @@
-import { cleanEnv, port, str } from "envalid";
+import { cleanEnv, port, str } from 'envalid';
 
 export const validateEnv = () => {
   cleanEnv(process.env, {
     JWT_SECRET: str(),
     PORT: port({ default: 8000 }),
-    MONGO_PATH: str(),
-    MONGO_USER: str(),
-    MONGO_PASSWORD: str()
+    POSTGRES_HOST: str(),
+    POSTGRES_PORT: str(),
+    POSTGRES_USER: str(),
+    POSTGRES_PASSWORD: str(),
+    POSTGRES_DB: str()
   });
 };
