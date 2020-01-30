a2b4bbb15d5580015f703b1e2c4ff6ae8ffc8bae
Frederick Johnson <fritzj96@gmail.com>
2020-01-29 18:47:26 -0800

    added: login method to userService (incomplete)

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,9 +2,11 @@ import * as bcrypt from "bcrypt";
 import * as jwt from "jsonwebtoken";
 import { getRepository } from "typeorm";
 import { EmailInUseException } from "../exceptions/emailInUse.exception";
+import { BadCredentialsException } from "../exceptions/BadCredentials.exception";
 import { User } from "./user.entity";
 import { CreateUserDTO } from "./createUser.dto";
 import { LoginUserDTO } from "./loginUser.dto";
+import { HttpException } from "src/exceptions/http.exception";
 
 export class UserService {
   private userRepository = getRepository(User);
@@ -26,6 +28,14 @@ export class UserService {
 
   public async login(userData: LoginUserDTO) {
     const user = await this.userRepository.findOne({ email: userData.email });
+    try {
+      const isMatch = await bcrypt.compare(userData.password, user.password);
+      if (!isMatch) {
+        throw new BadCredentialsException();
+      }
+    } catch {
+      throw new HttpException();
+    }
     user.password = undefined;
     const token = this.createToken(user);
     return { user, token };
