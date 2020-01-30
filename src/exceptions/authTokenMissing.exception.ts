53c34b78b915fc1bf09f2036871675ffeebea9af
Frederick Johnson <fritzj96@gmail.com>
2020-01-29 16:18:29 -0800

    added: better error response

--- src/exceptions/authTokenMissing.exception.ts
+++ src/exceptions/authTokenMissing.exception.ts
@@ -2,6 +2,6 @@ import { HttpException } from "./http.exception";
 
 export class AuthTokenMissingException extends HttpException {
   constructor() {
-    super(401, "Authentication Token Missing");
+    super(401, "Authentication token is missing");
   }
 }
